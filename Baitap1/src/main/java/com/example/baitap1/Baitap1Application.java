7e56f9ab3918d56be71a6ba12d457dcaa888f9d1
Trần Văn Khởi <tvkhoi123456@gmail.com>
2024-12-11 11:42:59 +0700

    Xây dựng ngay RESTful API để gửi lời chào đến khách hàng

---
+++ Baitap1/src/main/java/com/example/baitap1/Baitap1Application.java
@@ -0,0 +1,16 @@
+package com.example.baitap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baitap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baitap1Application.class, args);
+		System.out.println("Hello Khởi");
+
+	}
+
+
+}
