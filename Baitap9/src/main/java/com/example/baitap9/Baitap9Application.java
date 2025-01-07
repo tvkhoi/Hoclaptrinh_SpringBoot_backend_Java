756380b7e0ad979bb79d60e4d193a2d2d2ae8c11
Trần Văn Khởi <tvkhoi123456@gmail.com>
2025-01-07 16:17:12 +0700

    Mô tả chi tiết về bài tập

---
+++ Baitap9/src/main/java/com/example/baitap9/Baitap9Application.java
@@ -0,0 +1,13 @@
+package com.example.baitap9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baitap9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Baitap9Application.class, args);
+    }
+
+}
---
+++ Baitap9/src/test/java/com/example/baitap9/Baitap9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.baitap9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baitap9ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
